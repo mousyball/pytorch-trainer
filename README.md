06ff07656a874a1df492549a034d05b72218d223
mousyball <mouse801117@gmail.com>
2021-01-19 15:39:57 +0800

    [add] config and requirements.txt

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # pytorch-trainer
+
+## Requirements
+
+* `Python 3.7`
+
+```bash
+pip install -r requirements.txt
+```
---
+++ pytroch_trainer/utils/config.py
@@ -0,0 +1,14 @@
+from fvcore.common.config import CfgNode as CN
+
+_C = CN()
+
+_C.RUNNER = CN(new_allowed=True)
+_C.RUNNER.EPOCH = 100
+_C.RUNNER.ITERATION = 1000
+
+
+def get_cfg_defaults():
+  """Get a yacs CfgNode object with default values for my_project."""
+  # Return a clone so that the defaults will not be altered
+  # This is for the "local variable" use pattern
+  return _C.clone()
