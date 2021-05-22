3810541cf5de45a4fbeb821fa251e856ca13137b
janLin0817 <e8o1e8o1s7@gmail.com>
2021-05-22 16:54:26 +0800

    [feat] Distinguish train/val text logger
    * Update loss logger to one line, Before was multi-lines

--- pytorch_trainer/trainer/hooks/logger/text.py
+++ pytorch_trainer/trainer/hooks/logger/text.py
@@ -14,6 +14,12 @@ class TextLoggerHook(LoggerHook):
         self.interval = interval
         self.interval_cnt = 0
 
+    def before_train_epoch(self, trainer):
+        trainer.logger.info('=== {0:5s} section ==='.format('Train'))
+
+    def before_train_batch(self, trainer):
+        self.before_train_epoch(trainer)
+
     def after_train_iter(self, trainer):
         self.interval_cnt += 1
         if self.interval_cnt % self.interval != 0:
@@ -26,16 +32,31 @@ class TextLoggerHook(LoggerHook):
 
         self.interval_cnt = 0
 
+    def before_val_epoch(self, trainer):
+        trainer.logger.info('=== {0:5s} section ==='.format('Val'))
+
+    def before_val_batch(self, trainer):
+        self.before_val_epoch(trainer)
+
+    def after_val_epoch(self, trainer):
+        self._epoch_based_log(trainer)
+
+    def after_val_batch(self, trainer):
+        self._iter_based_log(trainer)
+
     def _epoch_based_log(self, trainer):
         step = trainer.epoch + 1
         loss_dict = self.get_loss_log(trainer)
+        loss_message = ''
         for key, val in loss_dict.items():
-            trainer.logger.info('epoch-{0} | iter-{1:6d} | {2:8s}:{3:5f}'.format(
-                step, trainer.inner_iter + 1, key, val))
+            loss_message += '| {0:8s}:{1:5f} '.format(key, val)
+        trainer.logger.info('epoch-{0} | iter-{1:6d} {2}|'.format(
+            step, trainer.inner_iter + 1, loss_message))
 
     def _iter_based_log(self, trainer):
         step = trainer.iter + 1
         loss_dict = self.get_loss_log(trainer)
+        loss_message = ''
         for key, val in loss_dict.items():
-            trainer.logger.info('| iter-{0:8d} | {1:8s}:{2:5f}'.format(
-                step, key, val))
+            loss_message += '| {0:8s}:{1:5f} '.format(key, val)
+        trainer.logger.info('| iter-{0:8d} {1}|'.format(step, loss_message))
