3763dd4302c7835ac9a56390e7512a6d6fa8a4db
mousyball <mouse801117@gmail.com>
2021-05-24 02:04:24 +0800

    [misc] Fix typo

--- pytorch_trainer/optimizers/regular.py
+++ pytorch_trainer/optimizers/regular.py
@@ -3,11 +3,11 @@ import torch.optim as optim
 from .builder import OPTIMIZERS
 
 # [NOTE] Pytorch official API
-torch_loss = {
+torch_optimizer = {
     'SGD': optim.SGD,
     'Adam': optim.Adam,
     'AdamW': optim.AdamW
 }
 
-for k, v in torch_loss.items():
+for k, v in torch_optimizer.items():
     OPTIMIZERS._do_register(k, v)
